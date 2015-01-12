de38de5811f95ac23b24342885b48b87a70f1f0a
nkasprak <nkasprak@gmail.com>
2015-01-12 17:54:34 -0500

    Color shading
    Needs some work

--- mapdrawer.js
+++ mapdrawer.js
@@ -10,63 +10,130 @@ var mapDrawer = function(mdl, svg) {
 			callback();
 		});
 	};
-	this.countiesByFIPS = (function() {
-		var toReturn = {};
+	this.RGBToHex = function (rgbArray) {
+		function pad(num, size) {
+			var s = "0" + num;
+			return s.substr(s.length - size);
+		}
+		return "#" + pad(Math.round(rgbArray[0]).toString(16), 2) + pad(Math.round(rgbArray[1]).toString(16), 2) + pad(Math.round(rgbArray[2]).toString(16), 2);
+	};
+	this.loopThroughCountiesInModel = function(countyHandler) {
 		for (var s in d.model.states) {(function(state) {
 			for (var c in state.counties) {(function(county, name) {
-				var cObj = {};
-				cObj.name = name;
-				for (var prop in county) {
-					cObj[prop] = county[prop];	
-				}
-				toReturn[county["FIPS"]] = cObj;
+				countyHandler(county,name);
 			})(state.counties[c], c);}
 		})(d.model.states[s]);}
+	};
+	this.countiesByFIPS = (function() {
+		var toReturn = {};
+		d.loopThroughCountiesInModel(function(county,name) {
+		var cObj = {};
+			cObj.name = name;
+			for (var prop in county) {
+				cObj[prop] = county[prop];	
+			}
+			toReturn[county["FIPS"]*1] = cObj;
+		});
 		return toReturn;
 	})();
+	this.getMergedSmallCountiesPath = function() {
+		var toReturn = {}, FIPSlist = {}, FIPSbyState = {};
+		for (var i = 0;i<d.paths.length;i++) {
+			FIPSlist[d.paths[i]["FIPS"]] = 1;
+		}
+		d.loopThroughCountiesInModel(function(county,name) {
+			var FIPS = county.FIPS;
+			delete FIPSlist[FIPS];
+		});
+		var stateFIPS;
+		for (var FIPS in FIPSlist) {
+			stateFIPS = Math.round((FIPS - FIPS%1000)/1000);
+			if (typeof(FIPSbyState[stateFIPS])=="undefined") FIPSbyState[stateFIPS] = [];
+			FIPSbyState[stateFIPS].push(FIPS*1);
+		};
+		var pathString;
+
+		for (stateFIPS in FIPSbyState) {
+			
+			pathString = "";
+			for (i=0;i<FIPSbyState[stateFIPS].length;i++) {
+				
+				FIPS = FIPSbyState[stateFIPS][i];
+				
+				if (d.pathsByFIPS[FIPS]) pathString += d.pathsByFIPS[FIPS].attr("d");	
+			}
+			toReturn[d.model.statesByFIPS[stateFIPS]] = pathString;
+		}
+		return toReturn;
+	};
 	this.storeData = function(p) {
 		this.paths = p;
 	};
 	this.pathsByFIPS = {};
 	this.draw = function() {
-		var numDots, FIPS;
+		function returnColor(uninsured,ppl) {
+			var color = 3*uninsured/ppl;
+			return d.RGBToHex([255*(1.1-color),255*(1-color),255*(1-1.1*color)]);
+		};
+		var numDots, FIPS, color;
 		var container = d3.select("#" + svg);
+		
 		this.canvas = container.append("svg:svg")
 			.attr("width",this.width)
-			.attr("height",this.width*0.7);
-		var scaleFactor = this.width/560;
+			.attr("height",this.width*0.6342)
+			.attr("viewBox","0 0 566 359");
+		d.dotList = [];
 		for (var i=0;i<this.paths.length;i++) {
-			FIPS = this.paths[i]["FIPS"];
+			FIPS = this.paths[i]["FIPS"]*1;
+			if (this.countiesByFIPS[FIPS]) {
+				color = returnColor(this.countiesByFIPS[FIPS]["uninsured"],this.countiesByFIPS[FIPS]["population"]);
+				console.log(color);
+			}
 			this.pathsByFIPS[FIPS] = this.canvas.append("svg:path")
 				.attr("d",this.paths[i]["path"])
-				.attr("stroke-width",0.25)
+				.attr("stroke-width",0)
 				.attr("stroke","#ddd")
-				.style("fill","#fff");
-				//.attr("transform","scale(" + scaleFactor + ")");
+				.style("fill",color);
 			if (this.countiesByFIPS[FIPS]) {
 				numDots = Math.round(this.countiesByFIPS[FIPS].uninsured/10000);
-				this.fillDots(numDots,FIPS,container);
+				this.calcDotCoords(numDots,FIPS);
 			}
-		};	
+		};
+		
+		d.mergedCountyPath = this.getMergedSmallCountiesPath();
+		for (var state in d.mergedCountyPath) {
+			
+			color = returnColor(d.model.states[state]["outCounty"]["uninsured"],d.model.states[state]["outCounty"]["population"]);
+			this.pathsByFIPS[d.model.states[state]["FIPS"]] = this.canvas.append("svg:path")
+				.attr("d",d.mergedCountyPath[state])
+				.attr("stroke-width",0)
+				.attr("stroke","#ddd")
+				.style("fill",color);
+			numDots = Math.round(d.model.states[state]["outCounty"]["uninsured"]/10000);
+			this.calcDotCoords(numDots,d.model.states[state]["FIPS"]);
+		};
+		this.drawDots();
 	};
-	this.fillDots = function(numDots, FIPS, container) {
-		function getPoints(path) {
-			var path = path.pathSegList;
-			var points = [];
-			for (var i=0;i<path.length;i++) {
-				points.push([path[i].x,path[i].y]);	
-			}
-			return points;
+	this.getPoints = function(path) {
+		var path = path.pathSegList;
+		var points = [];
+		for (var i=0;i<path.length;i++) {
+			points.push([path[i].x,path[i].y]);	
 		}
+		return points;
+	}
+	this.calcDotCoords = function(numDots, FIPS) {
+		
 		function fillPath(FIPS) {
 			if (d.pathsByFIPS[FIPS]) var path = d.pathsByFIPS[FIPS].node();
-			else return false;
-			var points = getPoints(path);
+			var points = d.getPoints(path);
 			var pointsInPolygon = 0;
 			var bbox = path.getBBox();
 			var randX,randY;
 			var usedPoints = [];
+			var errCounter = 0;
 			while (pointsInPolygon < numDots) {
+				
 				//Generate random point inside path bbox
 				var randX = Math.random()*bbox.width+bbox.x;
 				var randY = Math.random()*bbox.height+bbox.y;	
@@ -76,21 +143,28 @@ var mapDrawer = function(mdl, svg) {
 					usedPoints.push({x:randX,y:randY});
 					pointsInPolygon++;
 				}
-				if (pointsInPolygon > 100) break;
+				
+				errCounter++;
+				
+				if (errCounter > 100) break;
 			}
 			return usedPoints;
 		}
-		var dots = fillPath(FIPS);
-		var circle;
-		for (var i = 0; i<dots.length;i++) {
-			circle = d3.select(document.createElement("circle"))
-				.attr("cx", dots[i].x)
-				.attr("cy", dots[i].y)
-				.attr("r", 0.5)
-				.attr("fill","black");
-			d.canvas.node().appendChild(circle.node());
-		};
-		console.log(FIPS);
+		d.dotList = d.dotList.concat(fillPath(FIPS));
+	};
+	this.drawDots = function() {
+		var dots = d.dotList;
+		
+		var circle = this.canvas.selectAll("circle")
+			.data(dots)
+			.enter()
+			.append("circle");
+		
+		var attrs = circle
+			.attr("cx", function(d) {return d.x})
+			.attr("cy", function(d) {return d.y})
+			.attr("r", 0.5)
+			.attr("fill","#000");
 	};
 };
 
